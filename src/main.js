9d84373e469838029c4dbd64c0f8365f0e6c9741
Ewan <ewan.bains@gmail.com>
2018-11-03 15:25:06 +0000

    Basic implementation of sendIOTA

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import {iota, Converter} from './IOTAPackages.js';
+
+export let sendIOTA = function (seed,addr,amount,msg) {
+    const transfers = [{
+        address: addr,
+        value: amount, // 1Ki
+        tag: '', // optional tag of `0-27` trytes
+        message: Converter.asciiToTrytes(msg) // optional message in trytes
+    }]
+    iota.prepareTransfers(seed, transfers)
+        .then(trytes => {
+            // Persist trytes locally before sending to network.
+            // This allows for reattachments and prevents key reuse if trytes can't
+            // be recovered by querying the network after broadcasting.
+
+            // Does tip selection, attaches to tangle by doing PoW and broadcasts.
+            return iota.sendTrytes(trytes, depth, minWeightMagnitude)
+        })
+        .then(bundle => {
+            console.log(`Published transaction with tail hash: ${bundle[0].hash}`)
+            console.log(`Bundle: ${bundle}`)
+        })
+        .catch(err => {
+            // catch any errors
+        })
+}
